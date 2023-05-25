6b74613c1314512e1cfab838344f69565e6bd5f3
Brian Nguyen <briannguyen636@gnail.com>
2023-05-24 18:10:27 -0700

    Added Customer, Addresses, Customer Address, and Store Values

--- SetupTables.sql
+++ SetupTables.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS fast_food;
 CREATE DATABASE fast_food;
+GO
 USE fast_food;
 
 -- 3
@@ -15,13 +16,19 @@ CREATE TABLE Address (AddressID SERIAL PRIMARY KEY,
 INSERT INTO 
     Address(StreetAddress, City, ZIP, State, Country)
 VALUES
-
+	('9670 Lees Creek Dr.', 'Halethorpe', '21227', 'MD', 'United States'),
+	('83 Newport St.', 'Vista', '92083', 'CA', 'United States'),
+	('214 Linden Ave.', 'Elizabethton', '37643', 'TN', 'United States'),
+	('78 Glenwood Drive', 'Monroeville', '15146', 'PA', 'United States'),
+	('12 Mayflower Dr.', 'Stockbridge', '30281', 'GA', 'United States'),
+	('8462 Yukon Rd.', 'Poughkeepsie', '12601', 'NY', 'United States'),
+	('993 Princeton Ave.','Mason City', '50401', 'IA', 'United States')
 ;
 
 -- 5
 DROP TABLE IF EXISTS StoreBranch;
 CREATE TABLE StoreBranch (StoreNumber SERIAL PRIMARY KEY,
-                          AddressID BIGINT UNSIGNED NOT NULL,
+                          AddressID BIGINT NOT NULL,
                           Manager VARCHAR(255),
                           FOREIGN KEY(AddressID) REFERENCES Address(AddressID)
 );
@@ -29,7 +36,9 @@ CREATE TABLE StoreBranch (StoreNumber SERIAL PRIMARY KEY,
 INSERT INTO 
     StoreBranch(AddressID, Manager)
 VALUES
-
+	((SELECT AddressID FROM Address WHERE StreetAddress = '12 Mayflower Dr.'), 'Doctor Han'),
+	((SELECT AddressID FROM Address WHERE StreetAddress = '8462 Yukon Rd.'), 'Walter White'),
+	((SELECT AddressID FROM Address WHERE StreetAddress = '993 Princeton Ave.'), 'Satori Komeiji')
 ;
 
 -- 1
@@ -40,15 +49,18 @@ CREATE TABLE Customers (CustomerID SERIAL PRIMARY KEY,
 );
 
 INSERT INTO 
-    StoreBranch(Name)
+    Customers(Name)
 VALUES
-
+	('Raymoo Hakuray'),
+	('David Hayter'),
+	('Gordan Freeman'),
+	('Chen')
 ;
 
 -- 2
 DROP TABLE IF EXISTS CustomerAddress;
-CREATE TABLE CustomerAddress (CustomerID BIGINT UNSIGNED NOT NULL,
-                              AddressID BIGINT UNSIGNED NOT NULL,
+CREATE TABLE CustomerAddress (CustomerID BIGINT NOT NULL,
+                              AddressID BIGINT NOT NULL,
                               FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID),
                               FOREIGN KEY(AddressID) REFERENCES Address(AddressID)
 );
@@ -56,7 +68,14 @@ CREATE TABLE CustomerAddress (CustomerID BIGINT UNSIGNED NOT NULL,
 INSERT INTO 
     CustomerAddress(CustomerID, AddressID)
 VALUES
-
+	((SELECT CustomerID FROM Customers WHERE Name = 'Raymoo Hakuray'),
+		(SELECT AddressID FROM Address WHERE StreetAddress = '9670 Lees Creek Dr.')),
+	((SELECT CustomerID FROM Customers WHERE Name = 'David Hayter'), 
+		(SELECT AddressID FROM Address WHERE StreetAddress = '83 Newport St.')),
+	((SELECT CustomerID FROM Customers WHERE Name = 'Gordan Freeman'), 
+		(SELECT AddressID FROM Address WHERE StreetAddress = '214 Linden Ave.')),
+	((SELECT CustomerID FROM Customers WHERE Name = 'Chen'), 
+		(SELECT AddressID FROM Address WHERE StreetAddress = '78 Glenwood Drive'))
 ;
 
 DROP TABLE IF EXISTS PaymentMethod;
@@ -86,8 +105,8 @@ VALUES
 -- 6
 DROP TABLE IF EXISTS Orders;
 CREATE TABLE Orders (OrderNumber SERIAL PRIMARY KEY,
-                     StoreNumber BIGINT UNSIGNED NOT NULL,
-                     CustomerID BIGINT UNSIGNED NOT NULL,
+                     StoreNumber BIGINT NOT NULL,
+                     CustomerID BIGINT NOT NULL,
                      PickupMethod VARCHAR(255) NOT NULL,
                      PaymentMethod VARCHAR(255) NOT NULL,
                      OrderTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -134,11 +153,11 @@ VALUES
     ('Double Hamburger Combo', 9.99),
     ('Double Cheeseburger Combo', 9.99),
     ('Sausage Breakfast Combo', 7.99),
-    ('Bacon Breakfast Combo', 7.99),
+    ('Bacon Breakfast Combo', 7.99)
 ;
 
 DROP TABLE IF EXISTS EntreeItems;
-CREATE TABLE EntreeItems (ItemNumber BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE EntreeItems (ItemNumber BIGINT PRIMARY KEY,
                           FOREIGN KEY(ItemNumber) REFERENCES Items(ItemNumber)
 );
 
@@ -156,7 +175,7 @@ VALUES
 ;
 
 DROP TABLE IF EXISTS SideItems;
-CREATE TABLE SideItems (ItemNumber BIGINT UNSIGNED PRIMARY KEY
+CREATE TABLE SideItems (ItemNumber UNSIGNED PRIMARY KEY
                         FOREIGN KEY(ItemNumber) REFERENCES Items(ItemNumber)
 );
 
@@ -171,7 +190,7 @@ VALUES
 ;
 
 DROP TABLE IF EXISTS DrinkItems;
-CREATE TABLE DrinkItems (ItemNumber BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE DrinkItems (ItemNumber UNSIGNED PRIMARY KEY,
                          FOREIGN KEY(ItemNumber) REFERENCES Items(ItemNumber),
                          FOREIGN KEY(Ice) REFERENCES IceLevel(Level)
 );
@@ -197,7 +216,7 @@ VALUES
 ;
 
 DROP TABLE IF EXISTS LimitedItems;
-CREATE TABLE LimitedItems (ItemNumber BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE LimitedItems (ItemNumber BIGINT PRIMARY KEY,
                            Season VARCHAR(255),
                            FOREIGN KEY(Season) REFERENCES Seasons(Season),
                            FOREIGN KEY(ItemNumber) REFERENCES Items(ItemNumber)
@@ -212,7 +231,7 @@ VALUES
 
 -- 15
 DROP TABLE IF EXISTS BreakfastItems;
-CREATE TABLE BreakfastItems (ItemNumber BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE BreakfastItems (ItemNumber BIGINT PRIMARY KEY,
                              FOREIGN KEY(ItemNumber) REFERENCES Items(ItemNumber)
 );
 
@@ -225,10 +244,10 @@ VALUES
 ;
 
 DROP TABLE IF EXISTS Combos;
-CREATE TABLE Combos (ItemNumber BIGINT UNSIGNED PRIMARY KEY,
-                     EntreeItemNumber BIGINT UNSIGNED NOT NULL,
-                     SideItemNumber BIGINT UNSIGNED NOT NULL,
-                     DrinkItemNumber BIGINT UNSIGNED NOT NULL,
+CREATE TABLE Combos (ItemNumber BIGINT PRIMARY KEY,
+                     EntreeItemNumber BIGINT NOT NULL,
+                     SideItemNumber BIGINT NOT NULL,
+                     DrinkItemNumber BIGINT NOT NULL,
                      FOREIGN KEY(ItemNumber) REFERENCES Items(ItemNumber),
                      FOREIGN KEY(EntreeItemNumber) REFERENCES EntreeItems(ItemNumber),
                      FOREIGN KEY(SideItemNumber) REFERENCES SideItems(ItemNumber),
@@ -242,13 +261,13 @@ VALUES
         (SELECT ItemNumber FROM Items WHERE ItemName = 'Hamburger Combo'),
         (SELECT ItemNumber FROM Items WHERE ItemName = 'Hamburger'),
         (SELECT ItemNumber FROM Items WHERE ItemName = 'Small Fries'),
-        (SELECT ItemNumber FROM Items WHERE ItemName = 'Medium Drink'),
+        (SELECT ItemNumber FROM Items WHERE ItemName = 'Medium Drink')
     )
 ;
 
 DROP TABLE IF EXISTS OrderItems;
-CREATE TABLE OrderItems (OrderNumber BIGINT UNSIGNED NOT NULL,
-                         ItemNumber BIGINT UNSIGNED NOT NULL,
+CREATE TABLE OrderItems (OrderNumber BIGINT NOT NULL,
+                         ItemNumber BIGINT NOT NULL,
                          Quantity INT DEFAULT 1,
                          Modifications VARCHAR(255),
                          FOREIGN KEY(OrderNumber) REFERENCES Orders(OrderNumber),
@@ -263,7 +282,7 @@ VALUES
 
 -- 17
 DROP TABLE IF EXISTS ItemNutrition;
-CREATE TABLE ItemNutrition (ItemNumber BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE ItemNutrition (ItemNumber BIGINT PRIMARY KEY,
                             Calories INT,
                             Vegetarian INT DEFAULT 0,
                             Vegan INT DEFAULT 0
@@ -277,7 +296,7 @@ VALUES
 
 -- 16
 DROP TABLE IF EXISTS RewardItems;
-CREATE TABLE RewardItems (ItemNumber BIGINT UNSIGNED PRIMARY KEY,
+CREATE TABLE RewardItems (ItemNumber BIGINT PRIMARY KEY,
                           PointCost INT
 );
 
