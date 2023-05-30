9d9fd4eca5893da6c1d977dc48e5929f18802cda
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-30 16:40:38 -0700

    fix warnings

--- fast-food-app/src/components/RegularMenuTable.js
+++ fast-food-app/src/components/RegularMenuTable.js
@@ -1,31 +1,32 @@
 export default function RegularMenuTable(props) {
-    return (
-        <div className="table">
-            {props.loading ? (
-                <div>Loading...</div>
-            ) : (
-                <>
-                    <table border={1}>
-                        <tr>
-                            <th>Item #</th>
-                            <th>Item</th>
-                            <th>Price</th>
-                        </tr>
-                        {props.menu.items?.map (item => {
-                                return (
-                                    <tr>
-                                        <td>{item.ItemNumber}</td>
-                                        <td>{item.ItemName}</td>
-                                        <td>{item.Price}</td>
-                                    </tr>
-                                )
-                            }
-                        )}
-
-                    </table>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="table">
+      {props.loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <table border={1}>
+            <tbody>
+              <tr>
+                <th>Item #</th>
+                <th>Item</th>
+                <th>Price</th>
+              </tr>
+              {props.menu.items?.map (item => {
+                  return (
+                    <tr key={item.ItemNumber}>
+                      <td>{item.ItemNumber}</td>
+                      <td>{item.ItemName}</td>
+                      <td>{item.Price}</td>
+                    </tr>
+                  )
+                }
+              )}
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
 }
 
