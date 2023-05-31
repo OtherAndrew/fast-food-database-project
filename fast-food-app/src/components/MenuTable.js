894df21be6660614e5d5726176274ea05bacc40d
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-30 19:36:32 -0700

    refactor MenuTable.js

--- fast-food-app/src/components/MenuTable.js
+++ fast-food-app/src/components/MenuTable.js
@@ -1,25 +1,24 @@
 export default function MenuTable(props) {
-
-  return (
-    <div className="table">
-      {props.loading
-        ? (
-          <div>Loading...</div>
-        )
-        : (props.menu.combos
+  if (props.menu.combos) {
+    return (
+      <div className="table">
+        {props.loading
           ? (
+            <div>Loading...</div>
+          )
+          : (
             <table border={1}>
               <tbody>
-              <tr>
-                <th>Combo #</th>
-                <th>Combo</th>
-                <th>Entree</th>
-                <th>Side</th>
-                <th>Drink</th>
-                <th>Calories</th>
-                <th>Price</th>
-              </tr>
-              {props.menu.combos?.map(item => {
+                <tr>
+                  <th>Combo #</th>
+                  <th>Combo</th>
+                  <th>Entree</th>
+                  <th>Side</th>
+                  <th>Drink</th>
+                  <th>Calories</th>
+                  <th>Price</th>
+                </tr>
+                {props.menu.combos?.map(item => {
                   return (
                     <tr key={item.ComboNumber}>
                       <td>{item.ComboNumber}</td>
@@ -29,12 +28,21 @@ export default function MenuTable(props) {
                       <td>{item.Drink}</td>
                       <td>{item.Calories}</td>
                       <td>{item.Price}</td>
-                    </tr>
-                  );
+                    </tr>);
+                  })
                 }
-              )}
               </tbody>
             </table>
+          )
+        }
+      </div>
+    );
+  } else {
+    return (
+      <div className="table">
+        {props.loading
+          ? (
+            <div>Loading...</div>
           )
           : (
             <table border={1}>
@@ -46,22 +54,21 @@ export default function MenuTable(props) {
                 <th>Price</th>
               </tr>
               {props.menu.items?.map(item => {
-                  return (
-                    <tr key={item.ItemNumber}>
-                      <td>{item.ItemNumber}</td>
-                      <td>{item.ItemName}</td>
-                      <td>{item.Calories}</td>
-                      <td>{item.Price}</td>
-                    </tr>
-                  )
-                }
-              )}
+                return (
+                  <tr key={item.ItemNumber}>
+                    <td>{item.ItemNumber}</td>
+                    <td>{item.ItemName}</td>
+                    <td>{item.Calories}</td>
+                    <td>{item.Price}</td>
+                  </tr>);
+                })
+              }
               </tbody>
             </table>
           )
-        )
-      }
-    </div>
-  );
+        }
+      </div>
+    );
+  }
 }
 
