01473c40622594f0267843b3ebcefa780931dbca
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-31 22:35:24 -0700

    refactor OrderTable.js and SelectBox.js

--- fast-food-app/src/components/OrderTable.js
+++ fast-food-app/src/components/OrderTable.js
@@ -1,33 +1,32 @@
 export default function OrderTable(props) {
-    return (
-        <div>
-            
-            <table border={1}>
-                <tbody>
-                <tr>
-                    <th>OrderID</th>
-                    <th>Store #</th>
-                    <th>Customer ID</th>
-                    <th>Pickup</th>
-                    <th>Payment</th>
-                    <th>Time</th>
-                    <th>Total</th>
-                </tr>
-                {props.orders.orders?.map(item => {
-                    return (
-                    <tr key={item.OrderNumber}>
-                        <td>{item.OrderNumber}</td>
-                        <td>{item.StoreNumber}</td>
-                        <td>{item.CustomerID}</td>
-                        <td>{item.PickupMethod}</td>
-                        <td>{item.PaymentMethod}</td>
-                        <td>{item.OrderTime}</td>
-                        <td>{'$'+item.OrderTotal}</td>
-                    </tr>);
-                    })
-                }
-                </tbody>
-            </table>
-      </div>
-    );
+  const listItems = props.orders.orders?.map(item =>
+      <tr key={item.OrderNumber}>
+        <td>{item.OrderNumber}</td>
+        <td>{item.StoreNumber}</td>
+        <td>{item.CustomerID}</td>
+        <td>{item.PickupMethod}</td>
+        <td>{item.PaymentMethod}</td>
+        <td>{item.OrderTime}</td>
+        <td>{'$' + item.OrderTotal}</td>
+      </tr>
+  );
+
+  return (
+    <div>
+      <table border={1}>
+        <tbody>
+          <tr>
+            <th>OrderID</th>
+            <th>Store #</th>
+            <th>Customer ID</th>
+            <th>Pickup</th>
+            <th>Payment</th>
+            <th>Time</th>
+            <th>Total</th>
+          </tr>
+          {listItems}
+        </tbody>
+      </table>
+    </div>
+  );
 }
