808199a0f127406a06264208face9f0999c15f61
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-31 22:30:43 -0700

    refactor OrderItemTable.js

--- fast-food-app/src/components/OrderItemTable.js
+++ fast-food-app/src/components/OrderItemTable.js
@@ -1,27 +1,26 @@
 export default function OrderItemTable(props) {
-    return (
-        <div>
-            
-            <table border={1}>
-                <tbody>
-                <tr>
-                    <th>OrderID</th>
-                    <th>Item Name</th>
-                    <th>Quantity</th>
-                    <th>Modifications</th>
-                </tr>
-                {props.orders.items?.map(item => {
-                    return (
-                    <tr>
-                        <td>{item.OrderNumber}</td>
-                        <td>{item.ItemName}</td>
-                        <td>{item.Quantity}</td>
-                        <td>{item.Modifications}</td>
-                    </tr>);
-                    })
-                }
-                </tbody>
-            </table>
-      </div>
-    );
+  const listItems = props.orders.items?.map(item =>
+    <tr>
+      <td>{item.OrderNumber}</td>
+      <td>{item.ItemName}</td>
+      <td>{item.Quantity}</td>
+      <td>{item.Modifications}</td>
+    </tr>
+  );
+
+  return (
+    <div>
+      <table border={1}>
+        <tbody>
+          <tr>
+            <th>OrderID</th>
+            <th>Item Name</th>
+            <th>Quantity</th>
+            <th>Modifications</th>
+          </tr>
+          {listItems}
+        </tbody>
+      </table>
+  </div>
+  );
 }
